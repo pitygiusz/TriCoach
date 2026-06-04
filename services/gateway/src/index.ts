245c32b3ca67fc1adac4fb0ec7cddc7a61a9680a
Janek Wyrzykowski <janek.wyrzykowski@gmail.com>
2026-06-04 10:35:21 +0200

    undo last two commits

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -7,6 +7,6 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).send('<!doctype html><html><head><meta charset="utf-8"><title>Gateway</title></head><body><h1>hello world</h1></body></html>');
 });
 
-app.listen(Number(port), '0.0.0.0', () => {
+app.listen(Number(port), () => {
   console.log(`Gateway listening on port ${port}`);
 });
