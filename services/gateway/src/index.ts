50da5135af6673e8b42c05ba62ee5b5215710716
pitygiusz <piotrek@jurczyk.biz>
2026-06-04 14:05:55 +0200

    test gateway revert

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || '3000';
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('<!doctype html><html><head><meta charset="utf-8"><title>Gateway</title></head><body><h1>hello world</h1></body></html>');
+  res.status(200).send('<!doctype html><html><head><meta charset="utf-8"><title>Gateway</title></head><body><h1>hello world!</h1></body></html>');
 });
 
 app.listen(Number(port), () => {
