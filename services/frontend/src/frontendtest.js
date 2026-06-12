b95e17df8a63c64095a1e64405682f4868a63c1b
zochowskitomasz <tomaszowski13@o2.pl>
2026-06-12 11:13:16 +0200

    fix

--- services/frontend/src/frontendtest.js
+++ services/frontend/src/frontendtest.js
@@ -12,7 +12,7 @@ const toggle              = document.getElementById('toggle');
 const profileBtn          = document.getElementById('profileBtn');
 const overlay             = document.getElementById('overlay');
 
-// const defaultAvatar = `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><circle cx='12' cy='12' r='12' fill='%23e2e8f0'/><path d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z' fill='%2394a3b8'/></svg>`;
+const defaultAvatar = `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><circle cx='12' cy='12' r='12' fill='%23e2e8f0'/><path d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z' fill='%2394a3b8'/></svg>`;
 
 // Create-post modal
 const createPostModal     = document.getElementById('createPostModal');
